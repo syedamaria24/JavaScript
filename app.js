7a39305557a19608be720b3d0703eabc0b9e2e38
unknown <mariayousuf522@gmail.com>
2024-09-09 02:43:05 +0500

    added files

---
+++ app.js
@@ -0,0 +1,159 @@
+// CHAPTER # 1(ALERTS)---!
+
+// Q1:
+       // (i) 
+       alert("Maria");
+       // (ii)
+   alert("Yousuf");
+      // (iii)
+   alert("Dora123@gmail.com");
+      // (iv)
+   alert("01234567899");
+   
+   // Q2:
+   alert("You're learning JavaScript");
+   
+   // Q3:
+   alert("Welcome to our page!");
+   
+   // CHAPTER # 2(VARIABLES OF STRING)
+   
+   // Q1:
+   var fname = "Maria";
+   var lname = "Yousuf";
+   console.log(fname + " " + lname);
+   
+   // Q2:
+   var num1 = 20;
+   var num2 = "20";
+   console.log( num1 + 20);
+   
+   // Q3:
+   var teamname = "Your Team Name";
+   alert(teamname);
+   
+   // Q4:
+   var bestman = "Harry";
+   console.log(bestman);
+   
+   // CHAPTER # 3(VARIABLES OF NUMBERS)
+   
+   // Q1 & Q2:
+   var caseQty = 144;
+   console.log(caseQty);
+   
+   // Q3:
+   var num = 9;
+   var numAsNumber = Number(num);
+   var result = numAsNumber + 1;
+   console.log(num + " " + result)
+   
+   // Q4:
+   var sum = 2;
+   console.log(sum + 4);
+   
+   // Q5:
+   var merchTotal = 100;
+   var shippingCharge = 10;
+   var orderTotal = merchTotal + shippingCharge;
+   console.log(merchTotal + shippingCharge);
+   
+   // Q6:
+   let value = 10;
+   value += 5;
+   console.log(value)
+   
+   // CHAPTER 5(MATHS EXPRESSION)
+   
+   // Q1:
+   // var three = 3;
+   // var five = 5;
+   // var eight = 8; 
+   // document.write("Sum of" + " " + three + " " + "and" + " " + five + " " + "is" + " " + eight + );
+   // console.log(three + five + eight)
+   
+   // // Q2:
+   // console.log(three - five - eight)
+   // console.log(three * five * eight)
+   // console.log(three / five / eight)
+   
+   // // Q4:
+   // // Store the cost of one movie ticket in a variable
+   // var ticketPrice = 600; // Price in PKR
+   
+   // // Number of tickets to be bought
+   // var numberOfTickets = 5;
+   
+   // // Calculate the total cost
+   // var totalCost = ticketPrice * numberOfTickets;
+   
+   // // Output the result
+   // document.write("<br>" + "<br>" + "1. " + "Cost of one movie ticket: " + ticketPrice + " PKR" + "<br>" + "2. " + "Number of tickets: " + numberOfTickets + "<br>" + "3. " + "Total cost of buying " + numberOfTickets + " tickets: " + totalCost + " PKR"); 
+   
+   // Q5:
+   var num = 4;
+   document.write("<h1>Table of 4 <br></h1>");
+   document.write("4x1=" + num + "<br>"+ "4x2=" + num*2 + "<br>"+ "4x3=" + num*3 + "<br>" + "4x4=" + num*4 + "<br>" + "4x5=" + num*5 + "<br>" + "4x6=" + num*6 + "<br>" + "4x7=" + num*7 + "<br>" + "4x8=" + num*8 + "<br>" + "4x9=" + num*9 + "<br>" + "4x10="+ num*10)
+   
+   // Q6:
+   var C = 25;
+   var F = (C * 9 / 5)+ 32;
+   document.write( "<br>" + "<br>" + C + "<sup>0</sup> C is " + F + "<sup>o</sup> F <br>");
+   
+   F = 70;
+   var c = (F - 32)*5/9;
+   document.write(F + "<sup>0</sup> F  is "+ c + "<supo</sup> c. ")
+   
+   // Q7:
+   document.write("<h1>Shopping Cart</h1>");
+   
+   var priceofItem1 = 650, priceofItem2 = 100, OrderedquantityofItem1 = 3 , OrderquantityofItem2 = 7, shippingCharges = 100;
+   
+   document.write("Price of item 1 is = "+ priceofItem1 + "<br>" + "Quantity of item 1 is = " + OrderedquantityofItem1 + "<br>" + "Price of item 2 is = " + priceofItem2 + "<br>" + "Ordered Quantity of item 2 is = " + OrderquantityofItem2 + "<br>" + "Shipping Charges = " + shippingCharges + "<br> <br> <br>" + "Total cost of your order is = " + (priceofItem1 = OrderedquantityofItem1 + priceofItem2 * OrderquantityofItem2 + shippingCharges));
+   
+   // Q8: (MARKSHEET)
+   
+   document.write("<center<h1>MarkSheet</h1></center>");
+   var totalMarks = 980, marksObtained = 804;
+   
+   document.write("Total marks: "+ totalMarks + "<br>" + "Marks Obtained: " + marksObtained + "<br>" + "Percentage: " + 804/980*100 + "%" );
+   
+    // Q9: (CURRENCY CONVERTER)
+   
+   document.write("<center><h1>Currency in PKR</h1> </center><br>");
+   
+   var SAR = 28, USD = 104.80;
+   var PKR = USD*10 + SAR*25;
+   document.write("Total Currency in PKR: " + PKR );
+   
+   // Q10: (VARIABLE INITIALIZING)
+   
+   document.write("<center><h1>Variable Initializing</h1></center>");
+   
+   var initialNumber = 8
+   var result = ((initialNumber + 5) * 10) / 2;
+   
+   // Output the result
+   document.write("The result of the arithmetic operations is: " + result);
+   
+   
+   
+                        //  -----XXX--XXX----- //
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
